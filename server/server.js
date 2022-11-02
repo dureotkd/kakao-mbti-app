a6b1724016be38ebe3f4a2ac4afe7c5588a0f437
신성민 <dureotkd123@naver.com>
2022-11-02 19:39:30 +0900

    lasxzt

--- server/server.js
+++ server/server.js
@@ -9,9 +9,40 @@ app.get("/", (req, res) => {
   res.send("안녕하세요~!");
 });
 
-// ESTJ
 app.get("/mbti", (req, res) => {
-  console.log(req.query);
+  const mbti = req.query;
+
+  let result = "";
+
+  for (let key in mbti) {
+    const 객체 = mbti[key];
+
+    const [one, two] = Object.keys(객체);
+    const [oneVal, twoVal] = Object.values(객체);
+
+    if (oneVal >= twoVal) {
+      result += one;
+    } else {
+      result += two;
+    }
+
+    // if (value["I"] > value["E"]) {
+    //   result += "I";
+    // } else {
+    //   result += "E";
+    // }
+  }
+
+  console.log(result);
+
+  // console.log(mbti.for);
+  // mbti.forEach((item) => {
+  //   console.log(item);
+  // });
+
+  // console.log(req.query);
+
+  // ESFJ
 
   res.send("여기는 MBTI 결과값을 리턴해야합니다.");
 });
